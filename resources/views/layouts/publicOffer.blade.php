73dc313af43d7dae73be11e4387a7ed032fde899
carcusha <kuleshov.chyngyz@gmail.com>
2021-12-26 20:37:15 +0600

    publicOffer

--- resources/views/layouts/publicOffer.blade.php
+++ resources/views/layouts/publicOffer.blade.php
@@ -3,11 +3,11 @@
 @section('content')
     <main class="main">
         <div class="container main__content">
-            <div class="d-flex justify-content-between">
-                <div class="publicOffer"><h1>{!! ViewService::init()->type(['title'])->view('publicOffer') !!}</h1></div>
-                <a href="{{ url()->previous() }}" class="red-link">Вернуться назад</a>
+            <div class="d-flex main__content-head">
+                <h1 class="main__content-title">{!! ViewService::init()->type(['title'])->view('publicOffer') !!}</h1>
+                <a href="{{ url()->previous() }}" class="red-link ml-auto">Вернуться назад</a>
             </div>
-            <div class="publicOffer">{!! ViewService::init()->type(['text'])->view('publicOffer') !!}</div>
+            {!! ViewService::init()->type(['text'])->view('publicOffer') !!}
         </div>
     </main>
 
