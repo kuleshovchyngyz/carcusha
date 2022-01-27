6396dc13fd5fa2f986100c8e988298281bb67bc9
kuleshovchyngyz <kuleshovchyngyz@gmail.com>
2022-01-27 12:30:21 +0600

    fantom relations

--- app/Models/Fantom.php
+++ app/Models/Fantom.php
@@ -13,5 +13,14 @@ class Fantom extends Model
  *
  * @var array
  */
-protected $fillable = ['bitrix_lead_id'];
+    protected $fillable = ['bitrix_lead_id'];
+
+    public function User()
+    {
+        Lead::where('bitrix_lead_id',$this->bitrix_lead_id)->first();
+    }
+    public function lead()
+    {
+        return $this->hasOne(Lead::class,"bitrix_lead_id","bitrix_lead_id");
+    }
 }
