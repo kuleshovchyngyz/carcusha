028d954b49be27ab8114dc3c0c3e026b572a1232
kuleshovchyngyz <kuleshov.chyngyz@gmail.com>
2022-10-06 13:59:08 +0600

    firebaseToken

--- app/traits/MessageNotificationTrait.php
+++ app/traits/MessageNotificationTrait.php
@@ -24,7 +24,6 @@ trait MessageNotificationTrait
 //            Log::info('Deleted   event call: ' . $item);
         });
         static::created(function ($item) {
-
             $message = ['to'=> $item->user->firebase_token, 'notification'=>["title"=>$item->lead->vendor.' '.$item->lead->vendor_model.', '. $item->lead->vendor_year, "body"=> $item->message]];
             $res = Http::withHeaders(['Authorization'=>env('FIREBASE_TOKEN')])->post(env('FIREBASE_URL') ,$message);
         });
